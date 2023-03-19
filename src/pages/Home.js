dd928f3f40df2b4a240eda72508952a801373696
11chyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2023-03-19 17:53:34 +0900

    v6
    home 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,16 @@ import coffee from "../contents/coffee_bean2.svg";
 const Home = forwardRef((props, ref) => {
   return (
     <section ref={ref}>
-      <header>
-        <p className="logo">K A F E i n e</p>
-      </header>
       <div className="cv">
         <div className="content">
-          <h1 className="maintext">You Can't Quit Us!</h1>
+          <h1 className="maintext">KAFEine,</h1>
+          <h1 className="maintextEx">Brewing Everyday Moments</h1>
 
           <p className="text-need">
-            당신에게 꼭 필요한, <br /> &nbsp; &nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;없어서는 안 될{" "}
+            일상생활에서 특별한 순간들을 만들어 내기 위해
           </p>
           <div className="coffee-container">
-            <img className="coffee" src={coffee} />
+            <img className="coffee" src={coffee} alt=''/>
           </div>
         </div>
       </div>
